0668bb3b91f56b5eef29ac6cc77fa1cdf55119d2
Pawel L <virtuozoo@o2.pl>
2020-06-26 19:11:15 +0200

    fix logout

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../context/authContext';
 import { doSignOut } from '../firebase/auth';
 
 const Navigation = () => {
-  const currentUser = useContext(AuthContext);
+  const { currentUser }: any = useContext(AuthContext);
 
   const logoutHandler = () => {
     doSignOut();
