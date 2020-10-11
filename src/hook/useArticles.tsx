ac87ed681dca2c5da4ff0472307bb96ea4fc049f
Pawel L <virtuozoo@o2.pl>
2020-10-11 22:30:05 +0200

    add proxy url to fetch

--- src/hook/useArticles.tsx
+++ src/hook/useArticles.tsx
@@ -1,12 +1,14 @@
 import  { useState, useEffect } from 'react';
 
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/', ;
+
 export const useArticles = () => {
   const [article, setArticle] = useState([]);
   const [error, setError] = useState(false);
   useEffect(()  => {    
     const fetchData =  () => {
       if(process.env.REACT_APP_ARTICLES_URL && process.env.REACT_APP_ARTICLES_KEY) {
-        fetch(`${process.env.REACT_APP_ARTICLES_URL + process.env.REACT_APP_ARTICLES_KEY}`)
+        fetch(`${proxyUrl + process.env.REACT_APP_ARTICLES_URL + process.env.REACT_APP_ARTICLES_KEY}`)
           .then(data => data.json())
           .then(data => setArticle(data.articles.slice(0, 3)))
           .catch(err => setError(err));
